96f84b8bd2219e23eea149c902df3134ea236c09
DESKTOP-JHB2HP3\Loan <loan.janczak@gmail.com>
2017-07-15 16:07:19 +0200

    modif code source

--- src/CountAndSay.java
+++ src/CountAndSay.java
@@ -2,34 +2,33 @@
 public class CountAndSay {
 	public static String count(int n) {
 		String s = "1";
-		int j, k, nb;
-		int nbTot;
-		int len = 0;
-		while (n - 1 > 0) {
+		int len ;
+		int nb;
+		int  j;
+		while (n > 1) {
 			j = 0;
 			nb = 0;
-			k = 0;
-			
 			len = s.length();
-			nbTot = 0;
 			for (int i = 0; i < len; i++) {
-				if (len == 1 || i + 1 < len && s.charAt(i) != s.charAt(i + 1)) {			
-					nb = i - j + 1;
-					//s += "" + nb + s.charAt(j);
-					nbTot += nb + 1;
+				if (s.charAt(i) != s.charAt(j)) {
+					nb = i - j;
+					s += nb + "" + s.charAt(j);
+					j = 0;
+					i = -1;
+					s = s.substring(nb);
+					len = len - nb;
 					nb = 0;
 				}
 			}
-			s += "" + nb + s.charAt(j);
-			//s += "" + len + s.charAt(len - 1);
-			nbTot += len + 1;
-			s = s.substring(nbTot - 1);
+			nb = len;
+			s += nb + "" + s.charAt(j);
+			s = s.substring(nb);
 			n--;
 		}
 		return s;
 	}
-	
 	public static void main(String[] args) {
-		System.out.println(count(3));
+		int n = 5;
+		System.out.println(count(5));
 	}
 }
