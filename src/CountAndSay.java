2ce48c6019f0f6b4c7b09b8232de153d8987fecb
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-07-17 23:47:14 +0200

    Update CountAndSay.java
    10

--- src/CountAndSay.java
+++ src/CountAndSay.java
@@ -29,9 +29,7 @@ public class CountAndSay {
 	}
 	public static void main(String[] args) {
     //ok
-		// sth else 7
-		// again more 8 
-		// and 9,5
+		// 10
 		System.out.println(count(5));
 	}
 }
