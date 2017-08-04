6b05e40ab588d897bb8db55b5d4d7f5bf4a9d015
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-08-04 20:17:30 +0200

    new exercice

--- src/BinaryTreeBottomUpLevelOrderTraversal.java
+++ src/BinaryTreeBottomUpLevelOrderTraversal.java
@@ -1,50 +1,75 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class BinaryTreeBottomUpLevelOrderTraversal {
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
 	public class TreeNode {
-		private int val;
-		private TreeNode left;
-		private TreeNode right;
-		
-		public TreeNode(int v, TreeNode left, TreeNode right) {
-			this.val = v;
-			this.left = left;
-			this.right = right;
-		}
+		     int val;
+		     TreeNode left;
+		     TreeNode right;
+	
+		     public TreeNode(int val, TreeNode left, TreeNode right) {
+		    	 this.val = val;
+		    	 this.left = left;
+		    	 this.right = right;
+		     }
 	}
-	public static List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> all = new ArrayList<>();
-        List<Integer> rootList = new ArrayList<Integer>();
-        if (root != null) rootList.add(root.val);
-        else return all;
-        all.add(rootList);
-        process(root, all);
-        return reverseList(all);
-    }
-    private static List<List<Integer>> process(TreeNode n, List<List<Integer>> all) {
-        if (n == null) return null;
-        List<Integer> current = new ArrayList<>();
-        
-        if (n.left  != null) current.add(n.left.val);
-        if (n.right != null) current.add(n.right.val);
-        
-        if (!current.isEmpty()) all.add((List<Integer>) current);
-        
-        process(n.left, all);
-        process(n.right, all);
-        
-        return all;
-    }
-    private static List<List<Integer>> reverseList(List<List<Integer>> all) {
-        List<List<Integer>> rev = new ArrayList<>();
-        int size = all.size() - 1;
-        while (!all.isEmpty()) {
-            rev.add(all.remove(size));
-            size--;
-        }
-        return rev;
-    }
+    public static List<List<Integer>> levelOrderBottomCorr(TreeNode root) {
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+       List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+       
+       if(root == null) return wrapList;
+       
+       queue.offer(root);
+       while(!queue.isEmpty()){
+           int levelNum = queue.size();
+           List<Integer> subList = new LinkedList<Integer>();
+           for(int i=0; i<levelNum; i++) {
+               if(queue.peek().left != null) queue.offer(queue.peek().left);
+               if(queue.peek().right != null) queue.offer(queue.peek().right);
+               subList.add(queue.poll().val);
+           }
+           wrapList.add(0, subList);
+       }
+       return wrapList;
+   }
+   public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+           List<List<Integer>> all = new ArrayList<List<Integer>>();
+           Queue<TreeNode> q = new LinkedList<TreeNode>();
+       
+           if (root == null) return all;
+	        q.offer(root);
+       
+           int size = 0;
+           TreeNode n = null;
+           while(!q.isEmpty()) {
+               List<Integer> sub = new LinkedList<Integer>();
+               size = q.size();
+               
+               
+               for (int i = 0; i < size; i++) {
+                   n = q.peek();
+                   if (n.left  != null) q.offer(n.left);
+                   if (n.right != null) q.offer(n.right);
+                   Integer v = q.poll().val;
+                   if (v != null) sub.add(v);
+               }
+               all.add(0, sub); // ajoute en tête et non à la suite
+           }
+       return all;
+	}
+	
     public static void main(String[] args) {
     	BinaryTreeBottomUpLevelOrderTraversal tree = new BinaryTreeBottomUpLevelOrderTraversal();
     	TreeNode root = tree.new TreeNode(2, null, null);
@@ -52,6 +77,7 @@ public class BinaryTreeBottomUpLevelOrderTraversal {
     	root.left = tree.new TreeNode(3, null, null);
     	root.right = tree.new TreeNode(3, null, null);
     	
+    	/*
     	root.left.left = tree.new TreeNode(4, null, null);
     	root.left.right = tree.new TreeNode(5, null, null);
     	root.right.left = tree.new TreeNode(5, null, null);
@@ -61,8 +87,16 @@ public class BinaryTreeBottomUpLevelOrderTraversal {
     	root.left.right.right = tree.new TreeNode(9, null, null);
     	root.right.left.left = tree.new TreeNode(9, null, null);
     	root.right.left.right = tree.new TreeNode(8, null, null);
-    	
-    	 List<List<Integer>> result = levelOrderBottom(root);
+    	*/
+    	LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+    	 List<List<Integer>> all = new ArrayList<List<Integer>>();
+    	 Queue<TreeNode> q = new LinkedList<TreeNode>();
+    	queue.offer(null);
+    	queue.add(0, root);
+    	queue.peek();
+    	queue.remove();
+    	queue.poll();
+    	List<List<Integer>> result = levelOrderBottom(root);
     	System.out.println(result);
     }
 }
