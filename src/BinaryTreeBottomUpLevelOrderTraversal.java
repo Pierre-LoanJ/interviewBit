d5b566fdb04017d79db082b50ad6d80b0b3e37dc
DESKTOP-JHB2HP3\Loan <loan.janczak@gmail.com>
2017-08-08 01:04:34 +0200

    some new exercises

--- src/BinaryTreeBottomUpLevelOrderTraversal.java
+++ src/BinaryTreeBottomUpLevelOrderTraversal.java
@@ -4,6 +4,26 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+!! All the leafs are in the same array, as all nodes from the same level !!
+ */
 public class BinaryTreeBottomUpLevelOrderTraversal {
 	/**
 	 * Definition for a binary tree node.
---
+++ src/ConvertSortedArrayToBST.java
@@ -0,0 +1,60 @@
+/*
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+public class ConvertSortedArrayToBST {
+	public class TreeNode {
+		private TreeNode left;
+		private TreeNode right;
+		private int val;
+		
+		public TreeNode(int v) {
+			this.val = v;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	 public TreeNode sortedArrayToBST(int[] nums) {
+	        if (nums == null || nums.length == 0) return null; 
+	        int i = nums.length / 2;  
+	        TreeNode root = new TreeNode(nums[i]);
+	        feed(nums, root, i);
+	        return root;
+	    }
+	    private void feed(int[] a, TreeNode n, int i) {
+            int j = (i / 2) - 1;
+	        if (j < 0) return;
+	        n.left = new TreeNode(a[j]);
+            feed(a, n.left, j);
+            
+            i += ((a.length - i) / 2) + 1;
+	        if (i > a.length - 1) return;
+	        n.right = new TreeNode(a[i]);
+	        feed(a, n.right, i);
+	    }
+	    public TreeNode sortedArrayToBSTCorr(int[] num) {
+	        if (num.length == 0) {
+	            return null;
+	        }
+	        TreeNode head = helper(num, 0, num.length - 1);
+	        return head;
+	    }
+
+	    public TreeNode helper(int[] num, int low, int high) {
+	        if (low > high) { // Done
+	            return null;
+	        }
+	        int mid = (low + high) / 2;
+	        TreeNode node = new TreeNode(num[mid]);
+	        node.left = helper(num, low, mid - 1);
+	        node.right = helper(num, mid + 1, high);
+	        return node;
+	    }
+	    public static void main(String[] args) {
+	    	int[] a = { 1, 2, 3, 4, 5, 6, 7, }; //8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+	    	int[] b = {0};
+	    	ConvertSortedArrayToBST inst = new ConvertSortedArrayToBST();
+	    	TreeNode res = inst.sortedArrayToBSTCorr(a);
+	    	int c = 3;
+	    	
+	    }
+}
