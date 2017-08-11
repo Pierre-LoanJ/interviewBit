52518cdcd1fb8898f76b9612331fd74fefc82be3
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-08-11 18:37:20 +0200

    more code

--- src/ConvertSortedLinkedListInBalancedBST.java
+++ src/ConvertSortedLinkedListInBalancedBST.java
@@ -7,18 +7,42 @@ public class ConvertSortedLinkedListInBalancedBST {
 		      ListNode next;
 		      ListNode(int x) { val = x; }
 		  }
+	 public TreeNode sortedListToBSTCorr(ListNode head) {
+		    if(head==null) return null;
+		    return toBST(head,null);
+		}
+		public TreeNode toBST(ListNode head, ListNode tail){
+		    ListNode slow = head;
+		    ListNode fast = head;
+		    if(head==tail) return null;
+		    
+		    while(fast!=tail&&fast.next!=tail){
+		        fast = fast.next.next;
+		        slow = slow.next;
+		    }
+		    TreeNode thead = new TreeNode(slow.val);
+		    thead.left = toBST(head,slow);
+		    thead.right = toBST(slow.next,tail);
+		    return thead;
+		}
 	
-	
+		// does not work! because this tree is not balanced because 
+		/*				   2
+		 * 				 /
+		 * 			    0
+		 * 			  /	  \
+		 * 			-1	   1
+		 */
 	public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
-		TreeNode n = new TreeNode(head.val);
-		head = head.next;
+		if (head == null) return null;
+		TreeNode n = new TreeNode(head.val); // second miss!
+        head = head.next;                    // third miss!
         while(head != null) {
             n = putFather(n, head.val);
             head = head.next;
             if (head == null) return n;
             n.right = new TreeNode(head.val); // put as right child
-            head = head.next; // one miss
+            head = head.next;                 // one miss!
         }
         return n;
         
@@ -34,12 +58,15 @@ public class ConvertSortedLinkedListInBalancedBST {
     public static void main(String[] args) {
     	ConvertSortedLinkedListInBalancedBST ins = new ConvertSortedLinkedListInBalancedBST();
     	
-    	ListNode head = ins.new ListNode(-1); 
-    	head.next = ins.new ListNode(0); 
-    	head.next.next = ins.new ListNode(1);
-    	head.next.next.next = ins.new ListNode(2);
+    	ListNode head = ins.new ListNode(1); 
+    	head.next = ins.new ListNode(2); 
+    	head.next.next = ins.new ListNode(3);
+    	head.next.next.next = ins.new ListNode(4);
+    	head.next.next.next.next = ins.new ListNode(5);
+    	//head.next.next.next.next.next = ins.new ListNode(4);
+    	//head.next.next.next.next.next.next = ins.new ListNode(5);
     	
-    	TreeNode root = ins.sortedListToBST(head);
+    	TreeNode root = ins.sortedListToBSTCorr(head);
     	isBalancedBST isBal = new isBalancedBST();
     	
     	System.out.println(isBal.isBalanced(root));
