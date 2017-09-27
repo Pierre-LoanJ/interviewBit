891e3d53c0a6f9d27d6ea76d2fc082b539468d6e
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-09-27 14:00:51 +0200

    new

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerateParentheses {
+	public static List<String> generateParenthesis(int n) {
+        List<String> l = new LinkedList();
+        gen(l, "", n, 0);
+        return l;
+	}
+    private static void gen(List l, String s, int n, int m) {
+        if (n == 0 && m == 0) {
+        	l.add(s);
+        	return;
+        }
+        if (n > 0) {
+        	gen(l, s + "(", n-1, m+1);
+        }
+        if (m > 0) {
+        	gen(l, s + ")", n, m-1);
+        }   
+    }
+    public static void main(String[] args) {
+    	List<String> list = generateParenthesis(2);
+    	for (String word : list) {
+    		System.out.println(word);
+    	}
+    }
+}
--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -22,8 +22,26 @@ public class MajorityElement {
 	        return val;
 	    }
 	 // another solution (the clever one) is to use the Boyer-Moore voting algorithm.
+    public static int majorityElementMoore(int[] a) {
+        int major = 0, count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (count == 0) {
+                major = a[i];
+                count = 1;
+            }
+            //else is required, because a simple if would increment twice count.
+            else if (a[i] == major) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return major;
+    }
 	 public static void main(String[] args) {
 		 int[] a = { 1, 3, 7, 5, 8, 4, 5, 7, 6, 5, 8, 7, 9, 8, 7, 4, 5, 3, 4, 5, 7, 6 };
-		 System.out.println(majorityElement(a));
+		 int[] b = { 6, 5, 5};
+		 System.out.println(majorityElementMoore(b));
 	 }
 	}
